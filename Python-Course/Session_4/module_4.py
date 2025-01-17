3c0428d9dba846e06924927ed38963d18465f09f
olegA <olegarslanov@yahoo.com>
2025-01-17 10:08:58 +0200

    mhhm

--- Python-Course/Session_4/module_4.py
+++ Python-Course/Session_4/module_4.py
@@ -1,30 +1,45 @@
 PASSING_GRADE = 8
 
-
 class Trainee:
+    visited_lectures = 0
+    done_home_tasks = 0
+    missed_lectures = 0
+    missed_home_tasks = 0
+    mark = 0
+
     def __init__(self, name, surname):
-        pass
+        self.name = name
+        self.surname = surname
 
     def visit_lecture(self):
-        pass
+        self.visited_lectures += 1
+        self._add_points(1)
 
     def do_homework(self):
-        pass
+        self.done_home_tasks += 2
+        self._add_points(2)
 
     def miss_lecture(self):
-        pass
+        self.missed_lectures += 1
+        self._subtract_points(1)
 
     def miss_homework(self):
-        pass
+        self.missed_home_tasks += 2
+        self._subtract_points(2)
 
     def _add_points(self, points: int):
-        pass
+            self.mark = min(self.mark + points, 10)
 
     def _subtract_points(self, points):
-        pass
+            self.mark = max(self.mark - points, 0)
 
     def is_passed(self):
-        pass
+        self.missing_points = PASSING_GRADE - self.mark
+
+        if self.mark >= PASSING_GRADE:
+            print("Good job!")
+        else:
+            print(f"You need to {self.missing_points} points. Try to do your best!")
 
     def __str__(self):
         status = (
@@ -36,3 +51,12 @@ class Trainee:
             f"current mark {self.mark};\n"
         )
         return status
+
+
+trainee1 = Trainee('Oleg', 'Arslanov')
+trainee2 = Trainee('Lady', 'Brain')
+
+trainee1.visit_lecture()
+trainee1.do_homework()
+print(trainee1)
+trainee1.is_passed()
