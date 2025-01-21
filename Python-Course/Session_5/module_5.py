db4b248dc7838978dbf0f91d6e3e40f11d5aa22a
olegA <olegarslanov@yahoo.com>
2025-01-22 00:40:35 +0200

    nepolnyj no skolko uspel

--- Python-Course/Session_5/module_5.py
+++ Python-Course/Session_5/module_5.py
@@ -1,8 +1,9 @@
 # from collections import Counter
 import os
 from pathlib import Path
-# from random import choice
 from random import seed
+
+from random import choice
 from typing import List, Union
 
 # import requests
@@ -21,7 +22,28 @@ PATH_TO_STOP_WORDS = S5_PATH / "stop_words.txt"
 
 def task_1():
     seed(1)
-    pass
+    new_lst = []
+
+    with open("names.txt", "r", encoding="utf-8") as file:
+        names =  file.readlines()                                   # chtenije vseh strok v spisok
+
+    names = [name.strip().lower() for name in names]
+    sorted_names = sorted(names)                                    # poluchaju list
+
+    with open("last_names.txt", "r", encoding="utf-8") as file:
+        value = file.readlines()
+    last_names = [name.strip().lower() for name in value]
+
+    for x in sorted_names:
+        new_lst.append(x +" " + choice(last_names))
+
+    with open("sorted_names_and_surnames.txt", "w", encoding="utf-8") as file:
+        for i in new_lst:
+            file.write(i + "\n")
+
+task_1()
+        
+
 
 
 def task_2(top_k: int):
@@ -37,4 +59,17 @@ def task_4(data: List[Union[int, str, float]]):
 
 
 def task_5():
-    pass
+    a, b = input("Vvedite dve cifry cherez probel:").split()
+    try:
+        x = int(a) / int(b)
+    except ZeroDivisionError:
+        print("Can't divide by zero")
+    except ValueError:
+        print("Entered value is wrong")
+    else:
+        print(x)
+    finally:
+        print("Game Over")
+
+
+task_5()
