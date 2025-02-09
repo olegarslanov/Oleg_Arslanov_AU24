a545798ab1b0bdf07213eb4ebfd588531687e5b3
olegA <olegarslanov@yahoo.com>
2025-02-10 01:30:52 +0200

    update

--- DWH/Task7/HW7_BL_DM_without_insert.sql
+++ DWH/Task7/HW7_BL_DM_without_insert.sql
@@ -11,18 +11,37 @@ create table if not exists bl_dm.dim_dates (
     date_sur_id     bigint primary key,
     calendar_date   date not null,
     day_of_week     varchar(50) not null,
-    day             int not null,
-    month           int not null,
-    quarter         int not null,
-    year            int not null
+    day             int not null check (day > 0),
+    month           int not null check (month between 1 and 12),
+    quarter         int not null check (quarter between 1 and 4),
+    year            int not null check (year >= 0)
 );
+
+--drop table bl_dm.dim_dates cascade;
+
 
 -- add default values
 insert into bl_dm.dim_dates
 values (-1, '1970-01-01', 'n.a.', 1, 1, 1, 1970)
 on conflict do nothing;
 
---select * from bl_dm.dim_dates;
+
+-- code for automatic publicate of dates in dim_dates table
+
+--insert into bl_dm.DIM_DATES (date_sur_id, calendar_date, day_of_week, day, month, quarter, year)
+--select 
+	--cast(extract (year from dates) *10000 + extract(month from dates) *100 + extract(day from dates) as bigint) as date_sur_id,
+	--dates as calendar_date,
+	--to_char(dates, 'day') as day_of_week,
+	--extract(day from dates) as day,
+	--extract(month from dates) as month,
+	--extract (quarter from dates) as quarter,
+	--extract (year from dates) as year
+--from generate_series('2000-01-01'::date, '2029-12-31'::date, '1 day'::interval) as dates
+--where not exists (
+--select 1 from bl_dm.dim_dates where calendar_date = dates
+--);
+
 
 create table if not exists bl_dm.dim_customers_scd (
     customer_sur_id    bigint primary key,
@@ -33,14 +52,16 @@ create table if not exists bl_dm.dim_customers_scd (
     source_entity      varchar(255) not null,
     start_dt           timestamp not null,
     end_dt             timestamp not null,
-    is_active          varchar(1) not null,
-    insert_dt          timestamp not null
+    is_active          varchar(1) not null default 'y',
+    insert_dt          timestamp not null default current_timestamp,
+    constraint chk_dim_customers_scd_dates check (end_dt > start_dt)
 );
 
 -- add default values
 insert into bl_dm.dim_customers_scd 
 values (-1, 'n.a.', 'n.a.', 'n.a.', 'default', 'default', '1970-01-01 00:00:00', '9999-12-31 23:59:59', 'y', current_date)
 on conflict do nothing;
+
 
 
 create table if not exists bl_dm.dim_vendors (
@@ -117,18 +138,40 @@ values (-1, 'n.a.', 0.00, 'n.a.', 'default', 'default', current_date, current_da
 on conflict do nothing;
 
 
+-- create table of time 
 create table if not exists bl_dm.dim_time (
 	time_sur_id            bigint primary key,
 	calendar_time          time not null,
-	hour                   int not null,
-	minute                 int not null
+	hour                   int not null check (hour between 0 and 23) ,
+	minute                 int not null check (minute between 0 and 59)
 );
+
+--drop table bl_dm.dim_time cascade;
 
 --add default values
 insert into bl_dm.dim_time
 values (-1, '00:00:00', 0, 0)
 on conflict do nothing;
-	
+
+
+-- code for automatic publicate of dates interval in dim_time table
+
+--insert into bl_dm.DIM_TIME (time_sur_id, calendar_time, hour, minute)
+--select 
+	--cast(extract(hour from time_ ) * 100 + extract(minute from time_) as bigint) as time_sur_id,
+    --time_::time as calendar_time,  -- Convert back to 'time'
+    --extract(hour from time_) as hour,
+    --extract(minute from time_) as minute
+--from generate_series(
+    --'2000-01-01 00:00:00'::timestamp + (time_start - '00:00'::time), --here (time_start - '00:00'::time) we made interval, so we can add it to timestam value and get needed time from
+    --'2000-01-01 00:00:00'::timestamp + (time_end - '00:00'::time), 
+    --interval_minute
+--) as time_
+--where not exists (
+    --select 1 from bl_dm.DIM_TIME t where t.calendar_time = time_::time
+--);
+
+
 
 create table if not exists bl_dm.dim_junk_attributes (
 	junk_sur_id            bigint primary key,
@@ -168,7 +211,7 @@ create table if not exists bl_dm.fct_taxi_trips (
     payment_time_id       bigint not null, 
 	trip_duration         integer not null,
 	passenger_count       integer not null,
-	distance_miles        decimal(10,2) not null,
+	distance_miles        decimal(10,2) not null check (distance_miles >= 0),
 	trip_amount           decimal(10,2) not null,
 	update_dt             timestamp not null,
 	insert_dt             timestamp not null,
@@ -189,10 +232,6 @@ create table if not exists bl_dm.fct_taxi_trips (
 	constraint fk_dim_taxi_trips_pickup_time foreign key (pickup_time_id) references bl_dm.dim_time(time_sur_id),
 	constraint fk_dim_taxi_trips_dropoff_time foreign key (dropoff_time_id) references bl_dm.dim_time(time_sur_id),
 	constraint fk_dim_taxi_trips_payment_time foreign key (payment_time_id) references bl_dm.dim_time(time_sur_id)
-	------
-	--constraint fk_dim_taxi_trips_trip_duration foreign key (trip_duration) references bl_3nf.ce_taxi_trips(trip_duration),
-	--constraint fk_dim_taxi_trips_passenger_count foreign key (passenger_count) references bl_3nf.ce_taxi_trips(passenger_count),
-	--constraint fk_dim_taxi_trips_distance_miles foreign key (distance_miles) references bl_3nf.ce_taxi_trips(distance_miles)
 );
 	
 	--add default values
@@ -200,20 +239,86 @@ insert into bl_dm.fct_taxi_trips
 values (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0.00, 0.00, current_date, current_date)
 on conflict do nothing;
 
+-------------------------------------------------------------------
+--Add additional constraints
+
+
+--check dim_promotions discount percentage <= 100
+
+do $$
+begin
+if not exists (
+	select 1
+	from information_schema.table_constraints
+	where 
+		table_name = 'dim_promotions'
+		and constraint_name = 'bl_dm_check_value_0_100'
+) 
+then 
+	alter table bl_dm.dim_promotions
+	add constraint bl_dm_check_value_0_100 check (discount_percentage >= 0 and discount_percentage <= 100);
+end if;
+end $$;
+
+
+
+--check dim_customers_scd end_dt <start_dt
+
+do $$
+begin
+if not exists (
+	select 1
+	from information_schema.table_constraints
+	where 
+		table_name = 'dim_customers_scd'
+		and constraint_name = 'bl_dm_check_start_end_date'
+) 
+then 
+	alter table bl_dm.dim_customers_scd
+	add constraint bl_dm_check_start_end_date check (end_dt > start_dt);
+end if;
+end $$;
+
+--check ce_taxi_trips trip_duration > 0 and passenger_count > 0
+
+do $$
+begin
+if not exists (
+	select 1
+	from information_schema.table_constraints
+	where 
+		table_name = 'fct_taxi_trips'
+		and constraint_name = 'bl_dm_check_trip_duration'
+) 
+then 
+	alter table bl_dm.fct_taxi_trips
+	add constraint bl_dm_check_trip_duration check (trip_duration > 0 or trip_duration = -1);
+end if;
+
+if not exists (
+	select 1
+	from information_schema.table_constraints
+	where 
+		table_name = 'fct_taxi_trips'
+		and constraint_name = 'bl_dm_check_passenger_count'
+) 
+then 
+	alter table bl_dm.fct_taxi_trips
+	add constraint bl_dm_check_passenger_count check (passenger_count > 0 or passenger_count = -1);
+end if;
+end $$;
+
+----------------------------------------------------------------------------------------------------------
+--counting trip_amount
+--alter table bl_dm.fct_taxi_trips
+--add column trip_amount decimal(10,2) generated always as 
+    --(distance_miles * rate_per_mile + base_fare) stored;
+
+-- I understood that next we need to take data from 3NF and insert in Dim ...
 	
---finish transaction	
 commit;
+
 --rollback;
-	
-	
---select type(trip_duration) from bl_cl.combined_taxi_data ctd; 
-
-
-
-
---drop schema bl_dm cascade;
-
---select * from bl_dm.dim_vendors;
 
 
 
