aa920180b3456aa5abf0c70180639ca62df7a67c
olegA <olegarslanov@yahoo.com>
2025-04-05 18:51:30 +0300

    only STAGE1

--- Python-Course/Code_samples/greeter.py
+++
@@ -1,3 +0,0 @@
-def greet (person):
-    print(f"Hello, {person}")
-    print("Izmenenija ...")
--- Python-Course/Session_0/greeting.py
+++
@@ -1,5 +0,0 @@
-def greeting():
-    print("Hello. My name is Oleg Arslanov. Thanks in advance for my studies, Your time and knowledge!")
-
-
-greeting()
